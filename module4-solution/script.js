dfd27b512186f738d91beeed87b41bc499acdeee
Ayush Pokharel <ayushpokharel2184@gmail.com>
2018-10-07 18:11:58 +0530

    mod4_modified.

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -1,2 +1,15 @@
-var x="ayush";
-console.log(x);
+(function () {
+
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", 
+"Frank", "Larry", "Paula", "Laura", "Jim"];
+
+for (var i = 0; i < names.length; i++) {
+var firstLetter = names[i].charAt(0).toLowerCase();
+if (firstLetter === 'j') {
+    byeSpeaker.speak(names[i]);
+  } else {
+    helloSpeaker.speak(names[i]);
+  }
+}
+
+})();
